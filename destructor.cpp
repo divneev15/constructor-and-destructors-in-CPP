19a1840aa9d4cd288ab56e3514d7c763c897c302
divneev15 <divneevsingh123@gmail.com>
2025-08-31 15:00:54 +0530

    Create destructor.cpp

---
+++ destructor.cpp
@@ -0,0 +1,40 @@
+/*Divneev Singh
+24070123043
+ENTC A2*/
+
+#include <iostream>
+using namespace std;
+int count=0;
+class Books{
+    string title;
+    string author;
+    float price;
+
+    public:
+    Books(){
+        count++;
+        cout<<"No of objects created: "<<count<<endl;
+    }
+    ~Books(){
+        count--;
+        cout<<"No of objects remaining: "<<count<<endl;
+    }
+};
+int main(){
+    Books b1,b2,b3;
+    {
+        Books b4;
+    }
+    return 0;
+}
+
+/*Output
+No of objects created: 1
+No of objects created: 2
+No of objects created: 3
+No of objects created: 4
+No of objects remaining: 3
+No of objects remaining: 2
+No of objects remaining: 1
+No of objects remaining: 0
+*/
